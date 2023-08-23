d7c7832973e51dc704fffc5d6877e8a69ebaa5ae
bofa1ex <bofa1exx@gmail.com>
2023-08-23 13:51:40 +0800

    feat: spi module.  - version  - result value predicate for `BeanPropertyWriter` type-guard-checker.  - result handler predicate for `ResultHandler` type-guard-checker.

---
+++ maple-dsl-core/src/main/java/com/mapledsl/core/spi/MapleDslModule.java
@@ -0,0 +1,35 @@
+package com.mapledsl.core.spi;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class MapleDslModule {
+    @NotNull private final Class<? extends MapleDslResultHandler<?,?>> resultHandlerClazz;
+    @NotNull private final Class<?> resultValueClazz;
+    @NotNull private final String version;
+
+    public MapleDslModule(Class<?> resultValueClazz, Class<? extends MapleDslResultHandler<?,?>> resultHandlerClazz, String version) {
+        this.resultValueClazz = Objects.requireNonNull(resultValueClazz);
+        this.resultHandlerClazz = Objects.requireNonNull(resultHandlerClazz);
+        this.version = Objects.requireNonNull(version);
+    }
+
+    public Predicate<Class<?>> resultValuePredicate() {
+        return resultValueClazz::equals;
+    }
+
+    public Predicate<Class<? extends MapleDslResultHandler>> resultHandlerPredicate() {
+        return resultHandlerClazz::isAssignableFrom;
+    }
+
+    public Predicate<String> versionPredicate() {
+        return version::equalsIgnoreCase;
+    }
+
+    @Override
+    public String toString() {
+        return version;
+    }
+}
