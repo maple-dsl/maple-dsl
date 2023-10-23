d443f5e862f6c0bc736183e780dd33d2bfa4ada8
bofa1ex <bofa1exx@gmail.com>
2023-10-23 16:00:44 +0800

    chore: resultHandlerPredicate & resultValuePredicate in `MapleDslDuplexModule` without default impl.

--- maple-dsl-core/src/main/java/com/mapledsl/core/module/MapleDslDuplexModule.java
+++ maple-dsl-core/src/main/java/com/mapledsl/core/module/MapleDslDuplexModule.java
@@ -1,21 +1,8 @@
 package com.mapledsl.core.module;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.function.Predicate;
 
 public abstract class MapleDslDuplexModule extends MapleDslModule {
-    @SuppressWarnings("rawtypes")
-    public abstract @NotNull Class<MapleDslResultHandler> resultHandlerClazz();
-    public abstract @NotNull Class<?> resultValueClazz();
-
-    @Override
-    public Predicate<Class<?>> resultValuePredicate() {
-        return resultValueClazz()::equals;
-    }
-
-    @Override
-    public <IN, OUT> Predicate<MapleDslResultHandler<IN, OUT>> resultHandlerPredicate() {
-        return it -> it.getClass().isAssignableFrom(resultHandlerClazz());
-    }
+    public abstract <IN, OUT> Predicate<MapleDslResultHandler<IN, OUT>> resultHandlerPredicate();
+    public abstract Predicate<Class<?>> resultValuePredicate();
 }
