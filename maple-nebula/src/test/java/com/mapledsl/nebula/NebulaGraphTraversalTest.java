3f11feddaeae223a78d42830db09812fd6803130
bofa1ex <bofa1exx@gmail.com>
2023-10-23 16:03:19 +0800

    testcase: maple-nebula impact by adfa01cb.

--- maple-nebula/src/test/java/com/mapledsl/nebula/NebulaGraphTraversalTest.java
+++ maple-nebula/src/test/java/com/mapledsl/nebula/NebulaGraphTraversalTest.java
@@ -23,15 +23,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NebulaGraphTraversalTest {
-    static String formatSql(String origin) {
-        return origin.trim().replaceAll("\\s{2,}", " ");
-    }
-
     @BeforeAll
     public static void init() {
         new MapleDslConfiguration.Builder()
                 .module(MapleNebulaDslModule.class)
                 .templatePoolConfig(200, Runtime.getRuntime().availableProcessors() * 2, 2)
+                .templatePrettyPrint()
                 .namingStrategy(NamingStrategies.SNAKE_CASE)
                 .keyPolicyStrategy(KeyPolicyStrategies.MANUAL)
                 .dateFormatter("yyyy-MM-dd")
@@ -49,12 +46,9 @@ public class NebulaGraphTraversalTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER follow REVERSELY WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst")
+    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER follow REVERSELY WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id")
     public void should_traverse_use_out_vertex_id_as_default_output(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}").inE("follow").render())
-        );
+        assertEquals(expected, traverse("{{ vid }}").inE("follow").render());
     }
 
     @Test
@@ -69,40 +63,28 @@ public class NebulaGraphTraversalTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER * WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst")
+    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER * WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id")
     public void should_traverse_all_edge_type(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}").outE().render())
-        );
+        assertEquals(expected, traverse("{{ vid }}").outE().render());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER impact REVERSELY WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst")
+    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER impact REVERSELY WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id")
     public void should_traverse_detailed_relation_type(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}").inE(Impact.class).render())
-        );
+        assertEquals(expected, traverse("{{ vid }}").inE(Impact.class).render());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER impact,follow REVERSELY WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst")
+    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER impact,follow REVERSELY WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id")
     public void should_traverse_detailed_multi_relation_type(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}").inE(Impact.class, Follow.class).render())
-        );
+        assertEquals(expected, traverse("{{ vid }}").inE(Impact.class, Follow.class).render());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER impact REVERSELY WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst" +
-            " | GO 0 TO 1 STEPS FROM $-._dst OVER follow WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst")
+    @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER impact REVERSELY WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id" +
+            "| GO 0 TO 1 STEPS FROM $-.dst_id OVER follow WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id")
     public void should_traverse_multi_step(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}").inE(Impact.class).outE(Follow.class).render())
-        );
+        assertEquals(expected, traverse("{{ vid }}").inE(Impact.class).outE(Follow.class).render());
     }
 
     @Test
@@ -115,50 +97,44 @@ public class NebulaGraphTraversalTest {
             "WHERE $$.person.age < 30 AND ($$.person.name STARTS WITH \"bofa\" OR $$.person.name STARTS WITH \"zhangsan\") OR $$.person.age != 50 AND $$.person.aaaa != 1000 " +
             "YIELD $$.person.id AS id,$$.person.name AS name,$$.person.id AS dst_id")
     public void should_traverse_then_output_out_vertex_id(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}")
-                        .inE(Follow.class)
-                        .outV("p", Person.class, it -> it
-                                .lt("age", 30)
-                                .and(sub -> sub.startsWith("name", "bofa").or().startsWith("name", "zhangsan"))
-                                .or()
-                                .gt("age", 50)
-                                .gt("aaaa", 1000)
-                                .select("id", "name")
-                                .selectAs("id", "dst_id")
-                        )
-                        .render())
+        assertEquals(expected, traverse("{{ vid }}")
+                .inE(Follow.class)
+                .outV("p", Person.class, it -> it
+                        .lt("age", 30)
+                        .and(sub -> sub.startsWith("name", "bofa").or().startsWith("name", "zhangsan"))
+                        .or()
+                        .gt("age", 50)
+                        .gt("aaaa", 1000)
+                        .select("id", "name")
+                        .selectAs("id", "dst_id")
+                )
+                .render()
         );
     }
 
     @ParameterizedTest
     @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER follow REVERSELY " +
-            "WHERE 1==1 AND id($$) IS NOT NULL YIELD properties($^).id AS prop_id,id($^) AS real_vertex_id,id($$) AS next_traversal_id " +
+            "WHERE id($$) IS NOT NULL YIELD properties($^).id AS prop_id,id($^) AS real_vertex_id,id($$) AS next_traversal_id" +
             "| GO 0 TO 1 STEPS FROM $-.next_traversal_id OVER follow REVERSELY " +
-            "WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst,$-.prop_id,$-.real_vertex_id")
+            "WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id,$-.prop_id,$-.real_vertex_id")
     public void should_traverse_then_output_in_vertex_id(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}")
-                        .inE(Follow.class)
-                        .inV("p", it -> it.selectAs("id", "prop_id").selectAs(Model.ID, "real_vertex_id"))
-                        .outV("p2", it -> it.selectAs(Model.ID, "next_traversal_id"))
-                        .inE(Follow.class)
-                        .render())
+        assertEquals(expected, traverse("{{ vid }}")
+                .inE(Follow.class)
+                .inV("p", it -> it.selectAs("id", "prop_id").selectAs(Model.ID, "real_vertex_id"))
+                .outV("p2", it -> it.selectAs(Model.ID, "next_traversal_id"))
+                .inE(Follow.class)
+                .render()
         );
     }
 
     @ParameterizedTest
     @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER follow REVERSELY " +
-            "WHERE 1==1 AND id($$) IS NOT NULL YIELD properties(edge).type AS follow_type,id($$) AS _dst")
+            "WHERE id($$) IS NOT NULL YIELD properties(edge).type AS follow_type,id($$) AS dst_id")
     public void should_traverse_then_output_follow_type(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}")
-                        .inE(Follow.class)
-                        .edge("e", it -> it.selectAs("type", "follow_type"))
-                        .render())
+        assertEquals(expected, traverse("{{ vid }}")
+                .inE(Follow.class)
+                .edge("e", it -> it.selectAs("type", "follow_type"))
+                .render()
         );
     }
 
@@ -166,29 +142,25 @@ public class NebulaGraphTraversalTest {
     @ValueSource(strings = "GO 0 TO 1 STEPS FROM \"{{ vid }}\" OVER follow REVERSELY " +
             "WHERE $$.person.id == \"p001\" YIELD $$.person.id AS id,$$.person.name AS name,$$.person.name AS dup_person_name,$$.person.id AS dup_person_id")
     public void should_traverse_then_output_out_vertex_id_and_desc(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse("{{ vid }}")
-                        .inE(Follow.class)
-                        .outV("p", Person.class, it -> it
-                                .eq(Person::id, "p001")
-                                .select(Person::id, Person::getName)
-                                .selectAs(Person::getName, "dup_person_name")
-                                .selectAs(Person::id, "dup_person_id"))
-                        .render())
+        assertEquals(expected, traverse("{{ vid }}")
+                .inE(Follow.class)
+                .outV("p", Person.class, it -> it
+                        .eq(Person::id, "p001")
+                        .select(Person::id, Person::getName)
+                        .selectAs(Person::getName, "dup_person_name")
+                        .selectAs(Person::id, "dup_person_id"))
+                .render()
         );
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "LOOKUP ON person WHERE person.name == \"bofa\" YIELD id(vertex) as dst " +
-            "| GO 0 TO 1 STEPS FROM $-.dst OVER impact WHERE 1==1 AND id($$) IS NOT NULL YIELD id($$) AS _dst")
+    @ValueSource(strings = "LOOKUP ON person WHERE person.name == \"bofa\" YIELD id(vertex) as dst" +
+            "| GO 0 TO 1 STEPS FROM $-.dst OVER impact WHERE id($$) IS NOT NULL YIELD id($$) AS dst_id")
     public void should_traverse_via_match(String expected) {
-        assertEquals(
-                expected,
-                formatSql(traverse(vertex(Person.class)
-                        .eq(Person::getName, "bofa"))
-                        .outE(Impact.class)
-                        .render())
+        assertEquals(expected, traverse(vertex(Person.class)
+                .eq(Person::getName, "bofa"))
+                .outE(Impact.class)
+                .render()
         );
     }
 
