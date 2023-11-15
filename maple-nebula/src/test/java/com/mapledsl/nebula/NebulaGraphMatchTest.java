af69ec6fde8a7a7e060addf3b1ba7cdec9a83121
bofa1ex <bofa1exx@gmail.com>
2023-11-15 17:10:33 +0800

    [Nebula] testcase: match more cases.

--- maple-nebula/src/test/java/com/mapledsl/nebula/NebulaGraphMatchTest.java
+++ maple-nebula/src/test/java/com/mapledsl/nebula/NebulaGraphMatchTest.java
@@ -20,4 +20,41 @@ public class NebulaGraphMatchTest extends NebulaGraphBaseTest {
     public void should_match_edge_return_itself_automatic(String expected) {
         assertEquals(expected, edge(Impact.class).render());
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = "LOOKUP ON person WHERE person.name == \"bofa\" YIELD vertex AS v")
+    public void should_match_vertex_with_predicate(String expected) {
+        assertEquals(expected, vertex(Person.class)
+                .eq(Person::getName, "bofa")
+                .render());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "LOOKUP ON person WHERE person.name == \"bofa\" YIELD person.name AS v")
+    public void should_match_vertex_with_selection_complicit_alias(String expected) {
+        assertEquals(expected, vertex(Person.class)
+                .eq(Person::getName, "bofa")
+                .selectAs(Person::getName, "v")
+                .render());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "LOOKUP ON person WHERE person.name == \"bofa\" YIELD person.name AS v,person.age AS age | ORDER BY $-.v ASC,$-.age DESC")
+    public void should_match_vertex_with_selection_ordering(String expected) {
+        assertEquals(expected, vertex(Person.class)
+                .eq(Person::getName, "bofa")
+                .selectAs(Person::getName, "v").ascending()
+                .select(Person::getAge).descending()
+                .render());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "LOOKUP ON person WHERE person.name == \"bofa\" YIELD person.name AS v | OFFSET 5 LIMIT 10")
+    public void should_match_vertex_with_selection_limit(String expected) {
+        assertEquals(expected, vertex(Person.class)
+                .eq(Person::getName, "bofa")
+                .selectAs(Person::getName, "v")
+                .limit(5, 10)
+                .render());
+    }
 }
