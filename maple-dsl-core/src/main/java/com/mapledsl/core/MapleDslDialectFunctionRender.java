a8815a4d549e9cd970746c507f6ec5b4838ef302
bofa1ex <bofa1exx@gmail.com>
2023-11-20 12:27:08 +0800

    chore: function renderer with common toString and expose `toFunction` for subtype to impl.

--- maple-dsl-core/src/main/java/com/mapledsl/core/MapleDslDialectFunctionRender.java
+++ maple-dsl-core/src/main/java/com/mapledsl/core/MapleDslDialectFunctionRender.java
@@ -3,8 +3,24 @@ package com.mapledsl.core;
 import com.mapledsl.core.condition.wrapper.MapleDslDialectFunction;
 import org.stringtemplate.v4.AttributeRenderer;
 
+import java.util.Locale;
+
 public abstract class MapleDslDialectFunctionRender implements AttributeRenderer<MapleDslDialectFunction>, MapleDslDialectAware, MapleDslDialectContextAware, MapleDslDialectRenderHelper {
     protected MapleDslConfiguration context;
+    protected abstract String toFunction(MapleDslDialectFunction<?> value);
+
+    @Override
+    public String toString(MapleDslDialectFunction value, String formatString, Locale locale) {
+        if (value == null)  return NULL;
+
+        final String curFunction = toFunction(value);
+        if (!value.hasNext()) return curFunction;
+
+        final String nextFunction = toString(value.next, formatString, locale);
+        if (nextFunction.equals(NULL)) return curFunction;
+
+        return curFunction + COMMA + nextFunction;
+    }
 
     @Override
     public MapleDslDialectFunctionRender bind(MapleDslConfiguration context) {
--- maple-dsl-core/src/test/java/com/mapledsl/core/MapleDslDialectMockFunctionRender.java
+++ maple-dsl-core/src/test/java/com/mapledsl/core/MapleDslDialectMockFunctionRender.java
@@ -4,23 +4,13 @@ import com.mapledsl.core.condition.common.Func;
 import com.mapledsl.core.condition.wrapper.MapleDslDialectFunction;
 
 import java.util.EnumMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.function.UnaryOperator;
 
 public class MapleDslDialectMockFunctionRender extends MapleDslDialectFunctionRender implements MapleDslDialectRenderHelper {
 
     @Override
-    public String toString(MapleDslDialectFunction value, String formatString, Locale locale) {
-        if (value == null)  return NULL;
-
-        if (!value.hasNext()) return toFunction(value);
-        final String nextFunction = toString(value.next, formatString, locale);
-        if (nextFunction.trim().isEmpty()) return toFunction(value);
-        return toFunction(value) + COMMA + nextFunction;
-    }
-
-    private String toFunction(MapleDslDialectFunction<?> value) {
+    protected String toFunction(MapleDslDialectFunction<?> value) {
         if (value == null) return NULL;
         if (functionRenderMap.containsKey(value.func())) throw new UnsupportedOperationException("Unsupported Func: " + value.func().name());
         return functionRenderMap.get(value.func()).apply(value.column()) + AS + value.alias();
