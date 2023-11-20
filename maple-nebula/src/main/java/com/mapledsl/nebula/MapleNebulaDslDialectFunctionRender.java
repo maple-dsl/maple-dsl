cc78b809bc9dc75979cd24c648868c89216eb8a1
bofa1ex <bofa1exx@gmail.com>
2023-11-20 12:27:46 +0800

    [Nebula] squash(a8815a4d).

--- maple-nebula/src/main/java/com/mapledsl/nebula/MapleNebulaDslDialectFunctionRender.java
+++ maple-nebula/src/main/java/com/mapledsl/nebula/MapleNebulaDslDialectFunctionRender.java
@@ -6,26 +6,12 @@ import com.mapledsl.core.condition.wrapper.MapleDslDialectFunction;
 import com.mapledsl.nebula.module.MapleNebulaDslModule;
 
 import java.util.EnumMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.function.UnaryOperator;
 
 public class MapleNebulaDslDialectFunctionRender extends MapleDslDialectFunctionRender {
-
     @Override
-    public String toString(MapleDslDialectFunction value, String formatString, Locale locale) {
-        if (value == null)  return NULL;
-
-        final String curFunction = toFunction(value);
-        if (!value.hasNext()) return curFunction;
-
-        final String nextFunction = toString(value.next, formatString, locale);
-        if (nextFunction.equals(NULL)) return curFunction;
-
-        return curFunction + COMMA + nextFunction;
-    }
-
-    private String toFunction(MapleDslDialectFunction<?> value) {
+    protected String toFunction(MapleDslDialectFunction<?> value) {
         if (value == null) return NULL;
         if (!functionRenderMap.containsKey(value.func())) throw new UnsupportedOperationException("Unsupported Func: " + value.func().name());
         return functionRenderMap.get(value.func()).apply(value.column()) + AS + value.alias();
